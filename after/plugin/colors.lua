3ff9e279ec81daffb4097c82c2b8caf6242f62ac
jmattaa <mattajonathan1@gmail.com>
2023-08-26 17:05:36 +0200

    removed statusline highlight by default
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,6 +9,8 @@ function SetColorScheme(color) -- Color is the selected colorscheme
     -- Transparent bg
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    -- Transparent statusline
+    vim.api.nvim_set_hl(0, 'StatusLine', {bg = 'none'})
 end
 
 SetColorScheme('rose-pine')
