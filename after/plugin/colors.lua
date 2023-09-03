a49a0f2f90026b71d0d6c7d3c08f7435781da593
jmattaa <mattajonathan1@gmail.com>
2023-09-03 12:50:09 +0200

    Update nvim

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,7 +3,7 @@ require('rose-pine').setup({
 })
 
 function SetColorScheme(color) -- Color is the selected colorscheme
-	color = color or 'moonfly'
+	color = color or 'rose-pine'
 	vim.cmd.colorscheme(color)
 
     -- Transparent bg
