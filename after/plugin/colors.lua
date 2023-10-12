4e0055ea46e1e84009ec4132f4367cd1119c6099
jmattaa <mattajonathan1@gmail.com>
2023-10-12 18:02:41 +0200

    Made completion easier

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -9,4 +9,4 @@ function SetColorScheme(color) -- Color is the selected colorscheme
     -- vim.api.nvim_set_hl(0, 'StatusLine', {bg = 'none'})
 end
 
-SetColorScheme('cinnabar')
+SetColorScheme('moonfly')
