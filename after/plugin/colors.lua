0b08fcd4f96aab54a2ca27b6d3c08d1a81fc7952
jmattaa <mattajonathan1@gmail.com>
2023-10-21 16:16:59 +0200

    Update neovim colorscheme to transparent bg and cinnabar
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,10 +3,10 @@ function SetColorScheme(color) -- Color is the selected colorscheme
     vim.cmd.colorscheme(color)
 
     -- Transparent bg
-    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     -- Transparent statusline
-    -- vim.api.nvim_set_hl(0, 'StatusLine', {bg = 'none'})
+    vim.api.nvim_set_hl(0, 'StatusLine', {bg = 'none'})
 end
 
-SetColorScheme('moonfly')
+SetColorScheme('cinnabar')
