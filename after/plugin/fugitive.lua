ed449ad25fc86b6aa8b39b7a0196d15aadc8c635
jmattaa <mattajonathan1@gmail.com>
2023-12-08 19:10:42 +0100

    fix fugitive
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,6 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gg", ":G add .<CR>")
-vim.keymap.set("n", "<leader>gc", ":G commit -s<CR>")
-vim.keymap.set("n", "<leader>gp", ":G push<CR>")
+local opts = { silent = true }
+
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, opts)
+vim.keymap.set("n", "<leader>gg", ":G add .<CR>", opts)
+vim.keymap.set("n", "<leader>gc", ":G commit -s<CR>", opts)
+vim.keymap.set("n", "<leader>gp", ":G push<CR>", opts)
