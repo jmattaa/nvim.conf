534db4c76e5ef010dd48a9274c08b01ab32d7e81
jmattaa <mattajonathan1@gmail.com>
2023-12-08 19:09:27 +0100

    Add more fugitive keybinds
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gg", ":G add .<CR>")
+vim.keymap.set("n", "<leader>gc", ":G commit -s<CR>")
+vim.keymap.set("n", "<leader>gp", ":G push<CR>")
