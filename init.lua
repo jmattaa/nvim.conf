00532398ba011239af3f05d8ce27b2611e2fb409
jmattaa <mattajonathan1@gmail.com>
2023-12-31 15:40:43 +0100

    add multiple colorschemes functionality
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require("jonathan.set")
 require("jonathan.keymap")
 require("jonathan.statusline")
 require("jonathan.lazy")
+
+-- set our colorscheme
+require("jonathan.colorscheme").SetColorScheme("rose-pine")
