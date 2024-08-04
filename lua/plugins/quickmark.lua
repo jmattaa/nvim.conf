e6540c5f0bfe933eca341dca2aa80d2d0536a624
jmattaa <mattajonathan1@gmail.com>
2024-08-04 23:16:33 +0200

    not writing in the quickmark setup config cuz the default is the best :)
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- lua/plugins/quickmark.lua
+++ lua/plugins/quickmark.lua
@@ -1,18 +1,8 @@
 return {
     {
         "jmattaa/quickmark.nvim",
-        config = function()
-            require("quickmark").setup({
-                key_mappings = {
-                    list = '<leader>qq',
-                    add = '<leader>qa',
-                    remove = '<leader>qr',
-                    remove_all = '<leader>qR',
-                    save = '<leader>qs',
-                    shortcut = '<leader>qn',
-                    open_shortcut = '<leader>qo',
-                },
-            })
+        config = function ()
+            require"quickmark".setup()
         end
     }
 }
