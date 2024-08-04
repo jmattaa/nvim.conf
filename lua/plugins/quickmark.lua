e0a6a60998fb67a07faec7d39032dd6b5897c9f7
jmattaa <mattajonathan1@gmail.com>
2024-08-04 23:13:04 +0200

    update quickmark.lua plugin
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- lua/plugins/quickmark.lua
+++ lua/plugins/quickmark.lua
@@ -1,3 +1,18 @@
 return {
-    'jmattaa/quickmark.nvim'
+    {
+        "jmattaa/quickmark.nvim",
+        config = function()
+            require("quickmark").setup({
+                key_mappings = {
+                    list = '<leader>qq',
+                    add = '<leader>qa',
+                    remove = '<leader>qr',
+                    remove_all = '<leader>qR',
+                    save = '<leader>qs',
+                    shortcut = '<leader>qn',
+                    open_shortcut = '<leader>qo',
+                },
+            })
+        end
+    }
 }
