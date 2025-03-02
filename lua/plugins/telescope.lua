a13c7f711b84b475e2bf2ccdaadad39f9f693878
jmattaa <mattajonathan1@gmail.com>
2025-03-02 14:41:39 +0100

    find hidden files telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,13 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        require('telescope').setup()
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    hidden = true
+                }
+            }
+        })
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files)
