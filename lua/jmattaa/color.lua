0602cd41b32b50fb38e01da85953dda3e8e561fd
jmattaa <mattajonathan1@gmail.com>
2025-03-05 16:11:03 +0100

    refactor stuff
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- lua/jmattaa/color.lua
+++ lua/jmattaa/color.lua
@@ -1,13 +1,13 @@
 local function setColorscheme(color)
-    color = color or 'default'
-    vim.o.background = 'dark'
+    color = color or "default"
+    vim.o.background = "dark"
     vim.cmd.colorscheme(color)
 
     -- Transparent bg
-    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     -- Transparent statusline
-    -- vim.api.nvim_set_hl(0, 'StatusLine', {bg = 'none'})
+    -- vim.api.nvim_set_hl(0, "StatusLine", {bg = "none"})
 end
 
 return {
