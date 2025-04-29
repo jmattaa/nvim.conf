e15948ef0a2969886047a192bfff6c453a85671f
jmattaa <mattajonathan1@gmail.com>
2025-04-29 17:13:31 +0200

    change gruvbox plugin to ellisonleao/gruvbox.nvim

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -17,10 +17,13 @@ return {
         end
     },
     {
-        "morhetz/gruvbox",
-        config = function ()
-            vim.g.gruvbox_contrast_dark = "medium"
-            vim.g.gruvbox_sign_column = "bg0"
+        "ellisonleao/gruvbox.nvim",
+        config = function()
+            require("gruvbox").setup({
+                overrides = {
+                    SignColumn = { bg = "none" }
+                }
+            })
         end
     }
 }
