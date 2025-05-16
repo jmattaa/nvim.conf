19ad8da57c8c42a1ff85abd5a560d14eef56ad09
jmattaa <mattajonathan1@gmail.com>
2025-05-16 16:19:53 +0200

    add Jcmd command, opens a floating terminal with the output of a command

--- init.lua
+++ init.lua
@@ -5,3 +5,14 @@ require "jmattaa.color".setColorscheme("modus")
 
 local todo = require("jmattaa.todo")
 vim.api.nvim_create_user_command("JTodo", todo.toggle, {})
+
+local wutils = require("utils.window")
+vim.api.nvim_create_user_command("Jcmd", function(opts)
+    local cmd = opts.args or nil
+    if cmd == nil then
+        return
+    end
+
+    wutils.open_floating()
+    vim.fn.termopen(cmd)
+end, { nargs = "?" })
