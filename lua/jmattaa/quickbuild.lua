0f710b7939d119e6ee41739c313692b482cd92b4
jmattaa <mattajonathan1@gmail.com>
2025-08-11 23:42:04 +0200

    only start quickbuildls when quickbuild file is open

--- lua/jmattaa/quickbuild.lua
+++ lua/jmattaa/quickbuild.lua
@@ -1,29 +1,34 @@
 -- for the quickbuildls and the syntax highlighting for quickbuild
-
-local client = vim.lsp.start_client({
-    name = "quickbuildls",
-    cmd = {
-        "sh", "-c",
-        -- be sure to create a quickbuildls symlink
-        "$HOME/quickbuildls 2>> /tmp/quickbuildls.log"
-    },
-})
-
-if not client then
-    vim.notify("Failed to start quickbuildls", vim.log.levels.ERROR)
-    return
-end
-
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = { "quickbuild" },
     callback = function(args)
+        local clients = vim.lsp.get_clients({ name = "quickbuildls" })
+
+        ---@type integer?
+        local client_id = clients[1] and clients[1].id or nil
+        if not client_id then
+            client_id = vim.lsp.start({
+                name = "quickbuildls",
+                cmd = {
+                    "sh", "-c",
+                    -- be sure to create a quickbuildls symlink
+                    "$HOME/quickbuildls 2>> /tmp/quickbuildls.log"
+                },
+            })
+        end
+
+        if not client_id then
+            vim.notify("Failed to start quickbuildls", vim.log.levels.ERROR)
+            return
+        end
+
         local bufnr = args.buf
 
         -- hilighting
         vim.bo.filetype = "quickbuild"
 
         -- lsp
-        local ok = vim.lsp.buf_attach_client(bufnr, client)
+        local ok = vim.lsp.buf_attach_client(bufnr, client_id)
         if ok then
             vim.notify("Attached quickbuildls", vim.log.levels.INFO)
         else
