d2c554a9ae9014683240e19940cd8c7152e72d8c
jmattaa <mattajonathan1@gmail.com>
2025-08-11 11:47:21 +0200

    add <C-q> binding to mini.pick

--- lua/plugins/pick.lua
+++ lua/plugins/pick.lua
@@ -2,7 +2,30 @@ vim.pack.add({
     "https://github.com/echasnovski/mini.pick"
 })
 
-require("mini.pick").setup {}
+
+local MiniPick = require "mini.pick"
+MiniPick.setup {
+    mappings = {
+        mark_all = "<C-a>", -- default 
+        choose_marked = "<M-CR>", -- default 
+        open_quickfix_list = {
+            char = "<C-q>",
+            func = function()
+                vim.api.nvim_feedkeys(
+                    vim.api.nvim_replace_termcodes("<C-a>", true, false, true),
+                    'n',
+                    true
+                )
+                vim.api.nvim_feedkeys(
+                    vim.api.nvim_replace_termcodes("<M-CR>", true, false, true),
+                    'n',
+                    true
+                )
+            end
+        }
+    },
+}
+
 vim.keymap.set("n", "<leader>ff", ":Pick files<CR>")
 vim.keymap.set("n", "<leader>fg", ":Pick grep_live<CR>")
 vim.keymap.set("n", "<leader>fh", ":Pick help<CR>")
