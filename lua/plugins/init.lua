a46f02dcf99f44650117c815c797d7f772bd443c
jmattaa <mattajonathan1@gmail.com>
2025-12-26 18:43:00 +0100

    add some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,8 +8,8 @@ vim.pack.add {
     "https://github.com/Exafunction/codeium.vim",
     "https://github.com/windwp/nvim-autopairs",
     "https://github.com/jmattaa/fire.nvim",
+    "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 }
 
 require("fire").setup {}
-
 require("nvim-autopairs").setup {}
