6d307c5b418788292591a3fceb156ae88f2dc2a5
Jonathan Matta <mattajonathan1@gmail.com>
2026-06-23 12:51:23 +0200

    move back to modus from rainglow cuz it didn't really work well with my colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,12 @@
 vim.pack.add {
-    "https://github.com/rainglow/vim"
+    "https://github.com/vague2k/vague.nvim",
+    "https://github.com/miikanissi/modus-themes.nvim"
 }
 
-require "jmattaa.colorutils".setcolor "mud-contrast"
+require("vague").setup { terminal_colors = true }
+
+-- giving it a bit more contrast to work with ghostty
+require("modus-themes.colors").modus_vivendi.bg_dim = "#676767"
+require("modus-themes").setup { line_nr_column_background = false }
+
+require "jmattaa.colorutils".setcolor "modus"
