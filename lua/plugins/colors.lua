f7be861f7a411ba9a10deecc074ec8c5634c5062
Jonathan Matta <mattajonathan1@gmail.com>
2026-06-23 12:28:38 +0200

    use rainglow colors instead of multiple colorschemes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,5 @@
 vim.pack.add {
-    "https://github.com/vague2k/vague.nvim",
-    "https://github.com/miikanissi/modus-themes.nvim"
+    "https://github.com/rainglow/vim"
 }
 
-require("vague").setup { terminal_colors = true }
-
--- giving it a bit more contrast to work with ghostty
-require("modus-themes.colors").modus_vivendi.bg_dim = "#676767"
-require("modus-themes").setup { line_nr_column_background = false }
-
-require "jmattaa.colorutils".setcolor "modus"
+require "jmattaa.colorutils".setcolor "mud-contrast"
